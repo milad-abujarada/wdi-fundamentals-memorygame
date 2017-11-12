7c20da4735313a0e732ada77382b1675f40cd51b
Milad Abujarada <miladabujarada@c-76-120-31-92.hsd1.co.comcast.net>
2017-11-12 10:49:11 -0700

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,6 +1,17 @@
-var cardOne = 'queen';
-var cardTwo = 'king';
-var cardThree = 'queen';
-var cardFour = 'king';
-console.log("User flipped" + " " + cardTwo);
-console.log("User flipped" + " " + cardThree);
+var cards = ["queen", "queen","king", "king"];
+var cardsInPlay = [];
+var cardOne = cards[0];
+cardsInPlay.push(cardOne);
+var cardTwo = cards[1];
+cardsInPlay.push(cardTwo);
+if (cardsInPlay.length === 2) 
+{
+	if (cardsInPlay[0] === cardsInPlay[1]) 
+	{
+		alert("You have found a match!");
+	} 
+	else 
+	{
+		alert("Sorry, try again!");
+	}
+}
