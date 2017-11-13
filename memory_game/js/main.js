a2d9fe6f377c89af25748f143927a7d0c9a0f7e1
Milad Abujarada <miladabujarada@Milads-MacBook-Pro.local>
2017-11-13 07:03:05 -0700

    Add functions to main.js

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,17 +1,24 @@
 var cards = ["queen", "queen","king", "king"];
 var cardsInPlay = [];
-var cardOne = cards[0];
-cardsInPlay.push(cardOne);
-var cardTwo = cards[1];
-cardsInPlay.push(cardTwo);
-if (cardsInPlay.length === 2) 
+var checkForMatch = function()
 {
 	if (cardsInPlay[0] === cardsInPlay[1]) 
-	{
-		alert("You have found a match!");
-	} 
+		{
+			alert("You have found a match!");
+		} 
 	else 
+		{
+			alert("Sorry, try again!");
+		}
+};
+var flipCard = function(cardId)
+{
+	console.log("User flipped " + cards[cardId]);
+	cardsInPlay.push(cards[cardId]);
+	if (cardsInPlay.length === 2) 
 	{
-		alert("Sorry, try again!");
+		checkForMatch();
 	}
-}
+};
+flipCard(0);
+flipCard(2);
